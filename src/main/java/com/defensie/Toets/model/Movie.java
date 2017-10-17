7c6582c3678683ca7c510967d7485d449fb18b33
trozie <twanrozie@gmail.com>
2017-10-17 09:58:00 +0200

    Made POJO

--- src/main/java/com/defensie/Toets/model/Movie.java
+++ src/main/java/com/defensie/Toets/model/Movie.java
@@ -1,4 +1,37 @@
 package com.defensie.Toets.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String movieName;
+
+
+    // empty constructor
+    public Movie() {}
+
+
+    // getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
 }
