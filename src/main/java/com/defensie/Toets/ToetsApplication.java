101db1f4626b24a83a260dca6e610b38e0a6a9b3
trozie <twanrozie@gmail.com>
2017-10-17 09:22:14 +0200

    added index.html and trying to connect to github

---
+++ src/main/java/com/defensie/Toets/ToetsApplication.java
@@ -0,0 +1,12 @@
+package com.defensie.Toets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToetsApplication.class, args);
+	}
+}
