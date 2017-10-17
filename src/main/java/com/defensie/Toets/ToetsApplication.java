c16e75dafa71455cb980faa5a31cc8e09d86401e
trozie <twanrozie@gmail.com>
2017-10-17 09:53:42 +0200

    fixed App file, added basic annotations to Controller

--- src/main/java/com/defensie/Toets/ToetsApplication.java
+++ src/main/java/com/defensie/Toets/ToetsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ToetsApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  throws Exception {
 		SpringApplication.run(ToetsApplication.class, args);
 	}
 }
--- src/main/java/com/defensie/Toets/controller/MovieController.java
+++ src/main/java/com/defensie/Toets/controller/MovieController.java
@@ -1,4 +1,23 @@
 package com.defensie.Toets.controller;
 
+import com.defensie.Toets.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class MovieController {
+
+    @Autowired
+    MovieRepository movieRepository;
+
+    @RequestMapping(value = "/addMovie", method = RequestMethod.POST)
+    public Movie addMovie(@RequestBody Movie movie) {
+
+    }
+
+
 }
