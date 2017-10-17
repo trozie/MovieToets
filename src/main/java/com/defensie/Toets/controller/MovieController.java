f613cd528c3e7484eba45ad0ceb58db018684bbf
trozie <twanrozie@gmail.com>
2017-10-17 12:31:35 +0200

    added functionality's to controller

--- src/main/java/com/defensie/Toets/controller/MovieController.java
+++ src/main/java/com/defensie/Toets/controller/MovieController.java
@@ -5,6 +5,9 @@ import com.defensie.Toets.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class MovieController {
@@ -12,8 +15,22 @@ public class MovieController {
     @Autowired
     MovieRepository movieRepository;
 
+    @RequestMapping(value = "/getMovieList", method = RequestMethod.GET)
+    public Iterable<Movie> getAllMovies() {
+        List<Movie> list = new ArrayList<>();
+        movieRepository.findAll().forEach(list::add);
+        return list;
+    }
+
     @RequestMapping(value = "/addMovie", method = RequestMethod.POST)
     public Movie addMovie(@RequestBody Movie movie) {
+        return movieRepository.save(movie);
+    }
+
+    @RequestMapping(value = "/seenMovie", method = RequestMethod.POST)
+    public Movie seenMovie(@RequestBody int id) {
+        Movie movie = movieRepository.findOne(id);
+        movie.setMovieSeen(true);
         return movieRepository.save(movie);
     }
 
